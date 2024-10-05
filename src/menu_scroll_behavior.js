74dee4de8338bb235deafc8547cad771f51bcfbf
jaartola <56396786+jaartola@users.noreply.github.com>
2024-10-05 15:15:38 +0800

    update

--- src/menu_scroll_behavior.js
+++ src/menu_scroll_behavior.js
@@ -1,22 +1,20 @@
 (function () {
-  document.addEventListener('DOMContentLoaded', function () {
-    // Handle mobile menu scroll behavior
-    const body = document.body;
-    const menuButton = document.querySelector('.w-nav-button');
-    
-    if (menuButton) {
-      const menuObserver = new MutationObserver(function (mutations) {
-        mutations.forEach(function (mutation) {
-          if (
-            mutation.type === 'attributes' &&
-            mutation.attributeName === 'class'
-          ) {
-            const menuIsOpen = mutation.target.classList.contains('w--open');
-            body.style.overflow = menuIsOpen ? 'hidden' : 'auto';
-          }
-        });
+  // Handle mobile menu scroll behavior
+  const body = document.body;
+  const menuButton = document.querySelector('.w-nav-button');
+
+  if (menuButton) {
+    const menuObserver = new MutationObserver(function (mutations) {
+      mutations.forEach(function (mutation) {
+        if (
+          mutation.type === 'attributes' &&
+          mutation.attributeName === 'class'
+        ) {
+          const menuIsOpen = mutation.target.classList.contains('w--open');
+          body.style.overflow = menuIsOpen ? 'hidden' : 'auto';
+        }
       });
-      menuObserver.observe(menuButton, { attributes: true });
-    }
-  });
+    });
+    menuObserver.observe(menuButton, { attributes: true });
+  }
 })();
